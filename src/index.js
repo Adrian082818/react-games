ea3a114afa7abfbd2f8dde75f021c34c710417a0
Adrian082818 <mohabiradrian@gmail.com>
2020-05-12 15:47:36 -0400

    will add review reducer

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
 import * as serviceWorker from './serviceWorker';
+
+// const store = createStore(rootReducer, window.REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/reducers/reviews.js
@@ -0,0 +1,10 @@
+export default (state=[], action) => {
+    switch (action.type) {
+        case "LOAD_REVIEWS":
+            return action.reviews
+            case "ADD_REVIEW":
+                return state.concat(action.review)
+                default:
+                    return state;
+    }
+}
