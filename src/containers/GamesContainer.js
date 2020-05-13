99e2d4300154ebcd94626edd27e1dbd0e4c17047
Adrian082818 <mohabiradrian@gmail.com>
2020-05-13 10:51:19 -0400

    will add gamescontainer

---
+++ src/containers/GamesContainer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Game from '../components/Game.js';
+import ChooseGame from '../components/ChooseGame.js';
+import { connect } from 'react-redux';
+import { Switch, Route } from 'react-router-dom';
+
+const GamesContainer = ({ games }) => {
+    return (
+        <div className="GamesContainer">
+
+            <Switch>
+                <Route exact path='/games' component={ ChooseGame } />
+                <Route exact path='/games/:id' render={(routerProps) => {
+                    const game = games.find(game => game.id === parseInt(routerProps.match.params.id))
+                    return <Game {...routerProps} game={game} />
+                }} />
+            </Switch>
+        </div>
+    )
+}
+
+const mapStateToProps = ({ games }) => ({ games })
+
+export default connect(mapStateToProps)(GamesContainer);
